94fc1c3eeccc87fb89ed15e42cafaab124285984
mgh3326 <mgh3326@naver.com>
2018-06-20 13:32:37 +0900

    update

--- NodeExample1/ch03_test14.js
+++ NodeExample1/ch03_test14.js
@@ -5,7 +5,19 @@
  * slice() 메소드 사용
  */
 
-var Users = [{name:'소녀시대',age:20},{name:'걸스데이',age:22},{name:'티아라',age:23},{name:'애프터스쿨',age:25}];
+var Users = [{
+    name: '소녀시대',
+    age: 20
+}, {
+    name: '걸스데이',
+    age: 22
+}, {
+    name: '티아라',
+    age: 23
+}, {
+    name: '애프터스쿨',
+    age: 25
+}];
 
 console.log('배열 요소의 수 : %d', Users.length);
 console.log('원본 Users');
@@ -20,4 +32,3 @@ var Users3 = Users2.slice(1);
 
 console.log('slice()로 잘라낸 후 Users3');
 console.dir(Users3);
- 
